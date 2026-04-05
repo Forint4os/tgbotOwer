0c35c3413fc882aa64560cd32a977e4bbbde3259
Forint4os <alexandrov.maxim.1105@gmail.com>
2026-04-05 23:03:10 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+TOKEN = "8308924639:AAFFXbczSDEQB5ZbuRuXxhe5CaUM0DqOKVs"
+DEEPIKA_KEY = "sk-b4e0ab68700a436ca4d05d267280a29a"
+OWNER_USERNAME = "worhod"
+
+ADMINS = {"Админ1": None}  # Пока один админ
+DEPUTIES = {}  # Пока пусто, можно добавить позже
+CATEGORIES = [
+    "Работа", "Предложения", "Зарплата", "Выплата",
+    "Коллаба", "Ошибки", "Поддержка", "Личное", "Другое"
+]
