fb727afa77bed7e6e95c21b005be3d06b8bac735
Forint4os <alexandrov.maxim.1105@gmail.com>
2026-04-06 20:29:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 import asyncio
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
-from config import BOT_TOKEN
 from handlers import user, admin
+from config import TOKEN
 
 async def main():
-    bot = Bot(token=BOT_TOKEN, parse_mode="HTML")
-    storage = MemoryStorage()
-    dp = Dispatcher(storage=storage)
+    # Создаем бота и диспетчер
+    bot = Bot(token=TOKEN, parse_mode="HTML")
+    dp = Dispatcher(storage=MemoryStorage())
 
-    # Роутеры
+    # Подключаем маршруты
     dp.include_router(user.router)
     dp.include_router(admin.router)
 
-    print("🚀 Бот запущен")
+    print("🤖 Бот запущен!")
+    # Запуск polling
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
