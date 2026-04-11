8a8914c4c09a82069521daa9049610b07486b756
Forint4os <alexandrov.maxim.1105@gmail.com>
2026-04-12 01:12:05 +0200

    Add database initialization in main function
    Initialize the database during bot startup.

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import asyncio
+
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 
 from config import TOKEN
 from handlers import user, admin
+from database.db import init_db
+
 
 async def main():
+    # инициализация базы данных
+    init_db()
+
     bot = Bot(
         token=TOKEN,
         default=DefaultBotProperties(parse_mode="HTML")
@@ -13,12 +19,14 @@ async def main():
 
     dp = Dispatcher()
 
+    # подключаем роутеры
     dp.include_router(user.router)
     dp.include_router(admin.router)
 
     print("🤖 Бот запущен!")
 
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
