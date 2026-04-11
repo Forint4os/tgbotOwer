72a84cb698844c65a175a5a2cda39ce75080b940
Forint4os <alexandrov.maxim.1105@gmail.com>
2026-04-11 23:57:11 +0200

    Refactor bot initialization in main.py
    Refactor bot initialization to use DefaultBotProperties.

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import asyncio
 from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+import asyncio
 from aiogram.fsm.storage.memory import MemoryStorage
 from handlers import user, admin
 from config import TOKEN
 
 async def main():
     # Создаем бота и диспетчер
-    bot = Bot(token=TOKEN, parse_mode="HTML")
+   from aiogram.client.default import DefaultBotProperties
+
+bot = Bot(
+    token=TOKEN,
+    default=DefaultBotProperties(parse_mode="HTML")
+)
     dp = Dispatcher(storage=MemoryStorage())
 
     # Подключаем маршруты
