ec798bc5952dfbc3dfb95ec2decb9eb7f8e8432b
Forint4os <alexandrov.maxim.1105@gmail.com>
2026-04-12 00:30:35 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,9 @@
-# Токен бота
 TOKEN = "8308924639:AAFFXbczSDEQB5ZbuRuXxhe5CaUM0DqOKVs"
 
-# Токен нейросети DeepSika (если понадобится)
 DEEPIKA_KEY = "sk-b4e0ab68700a436ca4d05d267280a29a"
 
-# Юзеры
-OWNER_ID = 5476359789  # твой ID
+OWNER_ID = 5476359789
 
-# Администраторы: "Имя отображения": Telegram_ID
 ADMINS = [5476359789]
+
+ADMIN_ID = 5476359789
