ebbfd3af964c80f6a278fdc07243baee937ed842
Forint4os <alexandrov.maxim.1105@gmail.com>
2026-04-12 01:33:51 +0200

    Update tickets_kb.py

--- keyboards/tickets_kb.py
+++ keyboards/tickets_kb.py
@@ -5,9 +5,11 @@ def tickets_keyboard(tickets):
     buttons = []
 
     for t in tickets:
+        status = "❌" if t[5] == 0 else "✅"
+
         buttons.append([
             InlineKeyboardButton(
-                text=f"#{t[0]} | {t[3]} | @{t[2]}",
+                text=f"{status} #{t[0]} | {t[3]} | @{t[2]}",
                 callback_data=f"ticket_{t[0]}"
             )
         ])
