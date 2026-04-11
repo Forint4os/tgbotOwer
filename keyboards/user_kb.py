bc39068daa0af7f2dc7eb3e22d5c85f6cf4e7e8e
Forint4os <alexandrov.maxim.1105@gmail.com>
2026-04-12 01:45:46 +0200

    Update user_kb.py

--- keyboards/user_kb.py
+++ keyboards/user_kb.py
@@ -1,14 +1,35 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
 def main_menu():
-    return ReplyKeyboardMarkup(
-        keyboard=[
-            [KeyboardButton(text="📩 Написать"), KeyboardButton(text="🆘 Помощь")],
-            [KeyboardButton(text="💼 Работа"), KeyboardButton(text="💡 Предложения")],
-            [KeyboardButton(text="💰 Зарплата"), KeyboardButton(text="🤝 Коллаборация")],
-            [KeyboardButton(text="⚠️ Ошибки"), KeyboardButton(text="📞 Поддержка")],
-            [KeyboardButton(text="👤 Личное"), KeyboardButton(text="📦 Другое")]
+    return InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text="📩 Написать", callback_data="write")],
+        [InlineKeyboardButton(text="❓ Помощь", callback_data="help")]
+    ])
+
+
+def receiver_menu():
+    return InlineKeyboardMarkup(inline_keyboard=[
+        [
+            InlineKeyboardButton(text="👑 Admin", callback_data="rcv_admin"),
+            InlineKeyboardButton(text="💼 Работа", callback_data="rcv_work")
         ],
-        resize_keyboard=True
-    )
+        [
+            InlineKeyboardButton(text="💡 Предложения", callback_data="rcv_offer"),
+            InlineKeyboardButton(text="💰 Зарплата", callback_data="rcv_salary")
+        ],
+        [
+            InlineKeyboardButton(text="⚠️ Ошибки", callback_data="rcv_bug"),
+            InlineKeyboardButton(text="📞 Поддержка", callback_data="rcv_support")
+        ],
+        [
+            InlineKeyboardButton(text="👤 Личное", callback_data="rcv_private"),
+            InlineKeyboardButton(text="📦 Другое", callback_data="rcv_other")
+        ]
+    ])
+
+
+def back_menu():
+    return InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text="🔙 Назад", callback_data="back")]
+    ])
