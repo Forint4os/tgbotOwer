5f84c592f890f926f1ea91f1093e867ce57478fc
Forint4os <alexandrov.maxim.1105@gmail.com>
2026-04-12 12:11:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 import asyncio
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
+from aiohttp import ClientSession, TCPConnector, ClientTimeout
 
 from config import TOKEN
 from handlers import user, admin
 
 async def main():
+    timeout = ClientTimeout(total=60)
+
+    session = ClientSession(
+        connector=TCPConnector(limit=100),
+        timeout=timeout
+    )
+
     bot = Bot(
         token=TOKEN,
-        default=DefaultBotProperties(parse_mode="HTML")
+        default=DefaultBotProperties(parse_mode="HTML"),
+        session=session
     )
 
     dp = Dispatcher()
