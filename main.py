d36d319918bff72fbe7ca8a8d97c28fae60b4c52
Forint4os <alexandrov.maxim.1105@gmail.com>
2026-04-12 02:14:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,40 @@
 import asyncio
+import logging
 
 from aiogram import Bot, Dispatcher
+from aiogram.enums import ParseMode
 from aiogram.client.default import DefaultBotProperties
 
 from config import TOKEN
-from handlers import user, admin
 from database.db import init_db
 
+from handlers import user, admin
 
+
+# ---------------- LOGGING ----------------
+logging.basicConfig(level=logging.INFO)
+
+
+# ---------------- MAIN ----------------
 async def main():
+    # init DB
     init_db()
 
+    # bot
     bot = Bot(
         token=TOKEN,
-        default=DefaultBotProperties(parse_mode="HTML")
+        default=DefaultBotProperties(parse_mode=ParseMode.HTML)
     )
 
     dp = Dispatcher()
 
+    # routers
     dp.include_router(user.router)
     dp.include_router(admin.router)
 
     print("🤖 Bot started")
 
-    await dp.start_polling(
-        bot,
-        skip_updates=True,
-        allowed_updates=["message", "callback_query"]
-    )
+    await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
