77f091eb67bfa5e530b153db55c54f1f3def97dc
Forint4os <alexandrov.maxim.1105@gmail.com>
2026-04-26 02:56:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,62 @@
-import asyncio
+import logging
 from aiogram import Bot, Dispatcher
+from aiogram.types import Update
 from aiogram.client.default import DefaultBotProperties
-from aiohttp import ClientSession, TCPConnector, ClientTimeout
+from aiohttp import web
 
 from config import TOKEN
 from handlers import user, admin
 
-async def main():
-    timeout = ClientTimeout(total=60)
+# ТВОЙ CLOUDLFARE URL
+WEBHOOK_HOST = "rural-bacon-venice-convenience.trycloudflare.com"
+WEBHOOK_PATH = "/webhook"
+WEBHOOK_URL = f"https://{WEBHOOK_HOST}{WEBHOOK_PATH}"
 
-    session = ClientSession(
-        connector=TCPConnector(limit=100),
-        timeout=timeout
-    )
+WEBAPP_HOST = "0.0.0.0"
+WEBAPP_PORT = 3000
 
-    bot = Bot(
-        token=TOKEN,
-        default=DefaultBotProperties(parse_mode="HTML"),
-        session=session
-    )
+logging.basicConfig(level=logging.INFO)
 
-    dp = Dispatcher()
+bot = Bot(
+    token=TOKEN,
+    default=DefaultBotProperties(parse_mode="HTML")
+)
 
-    dp.include_router(user.router)
-    dp.include_router(admin.router)
+dp = Dispatcher()
 
-    print("🤖 Bot started")
+dp.include_router(user.router)
+dp.include_router(admin.router)
 
-    await dp.start_polling(bot)
+
+async def handle(request: web.Request):
+    data = await request.json()
+    update = Update(**data)
+    await dp.feed_update(bot, update)
+    return web.Response()
+
+
+async def on_startup(app):
+    await bot.delete_webhook(drop_pending_updates=True)
+    await bot.set_webhook(WEBHOOK_URL)
+    print(f"🚀 Webhook set: {WEBHOOK_URL}")
+
+
+async def on_shutdown(app):
+    await bot.delete_webhook()
+
+
+def main():
+    app = web.Application()
+
+    app.router.add_post(WEBHOOK_PATH, handle)
+
+    app.on_startup.append(on_startup)
+    app.on_shutdown.append(on_shutdown)
+
+    print("🤖 Bot started (WEBHOOK MODE)")
+
+    web.run_app(app, host=WEBAPP_HOST, port=WEBAPP_PORT)
+
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
